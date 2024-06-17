c6ee4d014ad65d15179c072f2f395a93ea8e2ce4
Mohammad Solki <53335722+msolki@users.noreply.github.com>
2024-06-17 19:15:05 +0200

    First commit, including the legacy web scraper and llm web scraper

---
+++ main.py
@@ -0,0 +1,10 @@
+# Test code to try ollama
+import ollama
+
+response = ollama.chat(model='llama3', messages=[
+  {
+    'role': 'user',
+    'content': 'Why is the sky blue?',
+  },
+])
+print(response['message']['content'])
